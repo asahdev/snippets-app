510d5bf72994987be662682e708d04296236e791
Anshul Sahdev <anshulsahdev@gmail.com>
2016-06-17 06:46:54 +0000

    Adding search function

--- snippets.py
+++ snippets.py
@@ -42,6 +42,13 @@ def catalog():
    logging.debug("all keywords displayed")
    return row
 
+def search(search_var):
+  """search for a keyword in snippets"""
+  with connection, connection.cursor() as cursor:
+    cursor.execute("select * from snippets where message like {!r}".format(search_var))
+    row = cursor.fetchone()
+    logging.debug("Snippet retrived successfully.")
+    return row
 
 #   logging.info("Funcation to get value of all keywords")
 #   cursor = connection.cursor()
@@ -70,25 +77,31 @@ def main():
     #Subparser for the all command
     logging.debug("Constructing all subparser")
     all_parser = subparsers.add_parser("all", help="show all keywords")
-#    all_parser.add_argument("all", help="Get values of all text")
 
+    #subparser for the search command
+    logging.debug("Constructing search subparser")
+    search_parser = subparsers.add_parser("search", help="search a keyword in messages")
+    search_parser.add_argument("search_var", help="Keyword to search")
 
 #    print(len(sys.argv))
 #    if len(sys.argv) <= 1:
 #      print("No argument provided")
     
-
-    arguments = parser.parse_args()
+    arguments = parser.parse_args(sys.argv[1:])
+#    arguments = parser.parse_args()
     arguments = vars(arguments)
     command = arguments.pop("command")
     if command == None:
-      print("No arguments entered")
+      print("Testing out")
     elif command == "put":
       name, snippet = put(**arguments)
       print("Stored {!r} as {!r}".format(snippet, name))
     elif command == "all":
-      keywords = catalog()
+      keywords = catalog(search_var)
       print("All Keyword are ",(keywords))
+    elif command == "search":
+      search_var = search(**arguments)
+      print("The result is ",(search_var))
     elif command == "get":
       snippet = get(**arguments)
       if snippet == None:
@@ -98,4 +111,4 @@ def main():
 
   
 if __name__ == "__main__":
-    main()
+  main()
